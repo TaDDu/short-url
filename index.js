46ef5f856d7aaac14bb31a19d6506b7a7675e7e5
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2019-04-20 23:10:49 +0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const eStatics = require("e-statics")(app);
+app.use(eStatics.counter);
+
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const crypto = require("crypto");
 const mongoose = require("mongoose");
 var validUrl = require("valid-url");
+
 var uri = process.env.mongourl;
 
-mongoose.connect(uri);
-app.use(bodyParser.json());
-app.use(morgan("tiny"));
-
+//mongoose.connect(uri,{autoReconnect:true});
 const urlSchema = mongoose.model("Url", {
   short: String,
   url: String,
   hits: Number
 });
+setTimeout(function() {
+  mongoose.connect(uri,function(err,succ){
+      if(err){
+          console.log("Error connecting to MongoDB:");
+          throw new Error(err);
+      }
+
+      console.log("succesfully connected to mongodb")
+  });
+}, 10000);
+
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
+// Health check
+app.get('/healthcheck', (req, res) => res.send());
 
 app.route("/new/:url(*)").get((req, res) => {
   if (validUrl.isUri(req.params.url)) {
@@ -30,7 +49,7 @@ app.route("/new/:url(*)").get((req, res) => {
     res.json({ error: true, message: "not valid url" });
   }
 });
-app.route("/new").post((req, res) => {
+app.post("/new",(req, res) => {
   var id = crypto.randomBytes(4).toString("hex");
   const url = new urlSchema({ short: id, url: req.body.url, hits: 0 })
     .save()
@@ -43,7 +62,7 @@ app.route("/statics/:id").get((req, res) => {
     res.json(urls);
   });
 });
-app.route("/:id").get((req, res) => {
+app.get("/:id",(req, res) => {
   urlSchema.findOneAndUpdate(
     { short: req.params.id },
     { $inc: { hits: 1 } },
